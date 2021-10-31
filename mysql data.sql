b3c6ff2401ca539f4c0632b1ce4a4ab2c130b478
A E S LOGHAPRIYA <91521856+loghapriya26@users.noreply.github.com>
2021-10-31 15:12:13 +0530

    Add files via upload

---
+++ mysql data.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM users.user
+LIMIT 0, 1000
+
+-- Date: 2021-10-31 15:11
+*/
+INSERT INTO `` (`ID`,`Firstname`,`Lastname`,`PhoneNumber`) VALUES (1,'Becham','Kelvin','9845612548');
+INSERT INTO `` (`ID`,`Firstname`,`Lastname`,`PhoneNumber`) VALUES (2,'George','Hack','9854656565');
+INSERT INTO `` (`ID`,`Firstname`,`Lastname`,`PhoneNumber`) VALUES (3,'John','Wick','8956548558');
